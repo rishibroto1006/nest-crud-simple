662538911cc07d022ebdb27cb59f1b1efdb27281
Rishi Broto Chakraborty <rishibroto.chakraborty@tectoro.com>
2024-01-16 16:38:20 +0530

    changes

--- src/students/student.controller.ts
+++ src/students/student.controller.ts
@@ -1,15 +1,14 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import { studentService } from './student.service';
-import { student } from '@prisma/client';
+import { Prisma, student } from '@prisma/client';
 
 @Controller('student')
 export class studentController {
   constructor(private studentService: studentService) {}
   @Post('create')
   async createStudent(
-    @Body() studentData: { name: string; age: number },
+    @Body() studentData: Prisma.studentCreateInput,
   ): Promise<student | null> {
-    const { name, age } = studentData;
     return this.studentService.createStudent(studentData);
   }
   @Get()
